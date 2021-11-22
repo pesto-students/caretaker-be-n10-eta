b52264d1bd010509ac9b13d1503efdda5c0f4163
Akshay Awasthi <akshayawasthi3@gmail.com>
2021-11-22 22:27:19 +0530

    old structure

--- index.js
+++ index.js
@@ -1,24 +1,24 @@
 require('dotenv').config()
 var loginRouter = require('./routes/login');
 var userRouter = require('./routes/user');
-// const app = require('./api/index');
-var express = require('express');
-var fileUpload = require('express-fileupload');
+// var express = require('express');
+// var fileUpload = require('express-fileupload');
 
-var cors = require("cors");
-var multer = require('multer');
-var upload = multer();
+// var cors = require("cors");
+// var multer = require('multer');
+// var upload = multer();
 var app = express();
 app.use(express.urlencoded({ extended: true, }));
-// app.use(upload.array()); 
-app.use(cors());
-app.use(express.json());
-app.use(fileUpload({
-    useTempFiles : true,
-    tempFileDir : '/tmp/'
-}));
-app.use('/', loginRouter);
-app.use('/', userRouter);
+const app = require('./api/index');
+// // app.use(upload.array()); 
+// app.use(cors());
+// app.use(express.json());
+// app.use(fileUpload({
+//     useTempFiles : true,
+//     tempFileDir : '/tmp/'
+// }));
+// app.use('/', loginRouter);
+// app.use('/', userRouter);
 var server = app.listen(process.env.PORT || 8081, function () {
     var host = server.address().address
     var port = server.address().port    
