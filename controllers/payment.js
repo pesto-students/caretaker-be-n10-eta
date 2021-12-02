71f96f11bd76bd7bd1ac57dfcad5ac098361e31a
Akshay Awasthi <akshayawasthi3@gmail.com>
2021-12-02 22:37:14 +0530

    order create in payment

--- controllers/payment.js
+++ controllers/payment.js
@@ -1,7 +1,26 @@
 require('dotenv').config()
-
-
+const { RAZORPAY_CONFIG } = process.env;
+var Razorpay = require('razorpay')
+var instance = new Razorpay({key_id: 'rzp_test_9hXnb5iQji6JR7',key_secret: 'QUhQn0GJQFHlaOchiDmYfvIH'});
 exports.make_payment = async function (req, res){
     const {body} = req;
-    console.log('body',body)
+    // console.log('body',body)
+    var options = {
+        amount: 50000,  // amount in the smallest currency unit
+        currency: "INR",
+        receipt: "order_rcptid_11"
+      };
+      instance.orders.create(options, function(err, order) {
+        console.log(order);
+        resp = {
+            status : true,
+            data : {
+                order_id : order.id,
+                all_data : order
+            }
+        }
+        
+        res.status(200);
+        res.json(resp);
+      });
 }
